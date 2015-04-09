98baeb7af9ba0e37db9b06474ed4af62d465df61
Javier Gusano Martinez <jgm1986@hotmail.com>
2015-04-09 10:43:03 +0000

    Webmin installation script.

---
+++ webmin/webmin_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+echo "***********************************************************"
+echo "* Webmin installation                                     *"
+echo "***********************************************************"
+cd ~
+echo "Starting Webmin installation..."
+sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python -y
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.740_all.deb
+sudo dpkg -i webmin_1.740_all.deb
+rm webmin_1.740_all.deb
+echo "Done!"
