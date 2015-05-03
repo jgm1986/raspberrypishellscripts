1c038eb10d04439d3b17876b5b1482f286135a9b
Javier Gusano Martinez <jgm1986@hotmail.com>
2015-05-03 08:36:52 +0000

    More dependeces in to Webmin installer for Linux RAID support.

--- webmin/webmin_install.sh
+++ webmin/webmin_install.sh
@@ -6,7 +6,18 @@ echo "* Webmin installation                                     *"
 echo "***********************************************************"
 cd ~
 echo "Starting Webmin installation..."
-sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python -y
+sudo apt-get install	perl				\
+			libnet-ssleay-perl		\
+			openssl				\
+			libauthen-pam-perl		\
+			libpam-runtime 			\
+			libio-pty-perl			\
+			apt-show-versions		\
+			python				\
+			libauthen-sasl-cyrus-perl	\
+			libauthen-sasl-perl		\
+			mdadm				\
+			-y
 wget http://prdownloads.sourceforge.net/webadmin/webmin_1.740_all.deb
 sudo dpkg -i webmin_1.740_all.deb
 rm webmin_1.740_all.deb
