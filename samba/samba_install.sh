ef9870d90ac575e76c11e8c934e4b65b50793569
Javier Gusano Martinez <jgm1986@hotmail.com>
2015-05-05 14:49:36 +0000

    Samba installation script.

---
+++ samba/samba_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+echo "***********************************************************"
+echo "* Samba Installer                                         *"
+echo "***********************************************************"
+
+sudo apt-get install	samba			\
+			samba-common-bin	\
+			-y
+
+echo "Setting parammeters for shared folder /home/pi/Descargas"
+# ------ EDIT THIS BLOCK  WITH YOUR OWN PREFERENCES------------
+sudo sed -i "\$a[Raspberry Pi Downloads]" /etc/samba/smb.conf
+sudo sed -i "\$acomment = Raspberry Pi Downloads" /etc/samba/smb.conf
+sudo sed -i "\$apath = /home/pi/Descargas" /etc/samba/smb.conf
+sudo sed -i "\$awriteable = Yes" /etc/samba/smb.conf
+sudo sed -i "\$acreate mask = 0777" /etc/samba/smb.conf
+sudo sed -i "\$adirectory mask = 0777" /etc/samba/smb.conf
+sudo sed -i "\$abrowseable = Yes" /etc/samba/smb.conf
+sudo sed -i "\$avalid users @users" /etc/samba/smb.conf
+sudo sed -i "\$aforce user = pi" /etc/samba/smb.conf
+sudo sed -i "\$ahide files = /lost+found/" /etc/samba/smb.conf
+# --------------------------------------------------------------
+echo "[ OK ]"
+
+sudo smbpasswd -a pi
+sudo /etc/init.d/samba restart
+echo "Done!"
