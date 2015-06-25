597f06e14a4f89c6738604da5686d45195d3bd24
Javier Gusano Martinez <jgm1986@hotmail.com>
2015-06-25 08:40:06 +0000

    Now the GitLab is restarted automatically.

--- gitlab/gitlab-copy-config.sh
+++ gitlab/gitlab-copy-config.sh
@@ -5,8 +5,11 @@ echo "* GitLab Copy Customize Config File                       *"
 echo "***********************************************************"
 echo "[INFO] Copy 'gitlab.rb.template' to '/etc/gitlab/gitlab.rb'"
 sudo cp gitlab.rb.template /etc/gitlab/gitlab.rb
-echo "OK"
+echo "[ OK ]"
 echo "[INFO] Reconfigure GitLab with the new cofig file..."
 sudo gitlab-ctl reconfigure
-echo "OK"
+echo "[ OK ]"
+echo "[INFO] Restarting GitLab with the new config..."
+sudo gitlab-ctl restart
+echo "[ OK ]"
 exit 0
