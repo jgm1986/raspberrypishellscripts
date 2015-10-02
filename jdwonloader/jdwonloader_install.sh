02735493b10cf308c61f89ee4a6fe3d4c503b9da
Javier Gusano Martinez <jgm1986@hotmail.com>
2015-10-02 16:12:19 +0000

    New JDownloader installation script.

---
+++ jdwonloader/jdwonloader_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+echo "***********************************************************"
+echo "* JDownloader Installer                                   *"
+echo "***********************************************************"
+cd $HOME
+mkdir JDownloader
+cd JDownloader
+wget http://installer.jdownloader.org/JDownloader.jar
+java -jar JDownloader.jar
+sudo chmod +x Core.jar
+echo "Done!"
+echo ""
+echo "Please insert the following line to your crontab to start"
+echo "JDownloader automatically at system startup."
+echo ""
+echo "@reboot java -jar /home/pi/JDownloader/Core.jar &"
