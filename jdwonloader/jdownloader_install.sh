9fc6630ae591d0a1d8b7566503e71a73f9231b58
Javier Gusano Martinez <jgm1986@hotmail.com>
2016-11-01 15:54:03 +0100

    Included JDownloader dependencies

--- jdwonloader/jdownloader_install.sh
+++ jdwonloader/jdownloader_install.sh
@@ -3,15 +3,16 @@ set -e
 echo "***********************************************************"
 echo "* JDownloader Installer                                   *"
 echo "***********************************************************"
+sudo apt-get install libjna-java -y
 cd $HOME
 mkdir JDownloader
 cd JDownloader
 wget http://installer.jdownloader.org/JDownloader.jar
 java -jar JDownloader.jar
-sudo chmod +x $HOME/JDownloader/Core.jar
+#sudo chmod +x $HOME/JDownloader/Core.jar
 echo "Done!"
 echo ""
 echo "Please insert the following line to your crontab to start"
 echo "JDownloader automatically at system startup."
 echo ""
-echo "@reboot java -jar /home/pi/JDownloader/Core.jar &"
+echo "@reboot java -jar /home/pi/JDownloader/JDownloader.jar &"
