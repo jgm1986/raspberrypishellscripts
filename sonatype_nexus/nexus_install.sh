79fb04a57c0921e00a9b52c9cc20d56a42dcecd2
Javier Gusano Martinez <jgm1986@hotmail.com>
2017-03-20 14:28:46 +0000

    Sonatype Nexus installer.

---
+++ sonatype_nexus/nexus_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+echo "***********************************************************"
+echo "* Sonatype Nexus Installer                                *"
+echo "***********************************************************"
+#sudo apt-get update
+#sudo apt-get install openjdk-8-jre -y
+
+mkdir -p $HOME/sonatype
+wget http://download.sonatype.com/nexus/3/latest-unix.tar.gz -O /tmp/nexus-latest-unix.tar.gz
+tar -C  $HOME/sonatype -xvf /tmp/nexus-latest-unix.tar.gz
+rm -f /tmp/nexus-latest-unix.tar.gz
+
+NEXUS_CONF="$HOME/sonatype/nexus-*/bin"
+rm $NEXUS_CONF/nexus.vmoptions
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $SCRIPT_DIR
+cp $SCRIPT_DIR/nexus.vmoptions $NEXUS_CONF
